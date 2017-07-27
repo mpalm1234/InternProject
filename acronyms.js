5c463d80bdd894642fcedbef7cedc5919b6c985d
mpalm1234 <mpalm@udel.edu>
2017-07-27 09:15:58 -0400

    Update acronyms.js

--- acronyms.js
+++ acronyms.js
@@ -1,209 +1,225 @@
-// Acronyms:
+var Alexa = require('alexa-sdk');
+
+    exports.handler = function(event, context, callback) {
+        var alexa = Alexa.handler(event, context);
+        alexa.registerHandlers(handlers);
+        alexa.execute();
+    };
+
+    var handlers = {
+        
+        'LaunchRequest': function () {
+            this.emit('MyIntent');
+        },
+
         'acronymHelper': function () {
             
-            var say = "";
+            var acronym = this.event.request.intent.slots.AcronymName.value;
+            var say = "Sorry, I don't know that acronym.";
             
-            if (acronym == 'A D R'){
+            switch(acronym) {
+            case 'A D R':
                 say = 'A D R stands for Application Design Review';
-            }
-            else if (acronym == 'A P O'){
+                break;
+            case 'A P O':
                 say = 'A P O stands for Application Project Office';
-            }
-            else if (acronym == 'A R D'){
+                break;
+            case 'A R D':
                 say = 'A R D stands for Application Requirements Document';
-            }
-            else if (acronym == 'A R T'){
+                break;
+            case 'A R T':
                 say = 'A R T stands for All Requirements and Testing';
-            }
-            else if (acronym == 'A S D'){
+                break;
+            case 'A S D':
                 say = 'A S D stands for Accounting Services Department';
-            }
-            else if (acronym == 'A S O'){
+                break;
+            case 'A S O':
                 say = 'A S O stands for Application Support Office';
-            }
-            else if (acronym == 'B I'){
+                break;
+            case 'B I':
                 say = 'B I stands for Business Intelligence';
-            }
-            else if (acronym == 'B P'){
+                break;
+            case 'B P':
                 say = 'B P stands for Business Partner';
-            }
-            else if (acronym == 'B P M'){
+                break;
+            case 'B P M':
                 say = 'B P M stands for Business Process Management';
-            }
-            else if (acronym == 'B R'){
+                break;
+            case 'B R':
                 say = 'B R stands for Business Requirements';
-            }
-            else if (acronym == 'B V A'){
+                break;
+            case 'B V A':
                 say = 'B V A stands for Business Value Assessment';
-            }
-            else if (acronym == 'B V S'){
+                break;
+            case 'B V S':
                 say = 'B V S stands for Business Valution Summary';
-            }
-            else if (acronym == 'C B T'){
+                break;
+            case 'C B T':
                 say = 'C B T stands for Computer Based Training';
-            }
-            else if (acronym == 'C I'){
+                break;
+            case 'C I':
                 say = 'C I stands for Configuration Management';
-            }
-            else if (acronym == 'C I S O'){
+                break;
+            case 'C I S O':
                 say = 'C I S O stands for Chief Information Security Office';
-            }
-            else if (acronym == 'C M D B'){
+                break;
+            case 'C M D B':
                 say = 'C M D B stands for Configuration Management Database';
-            }
-            else if (acronym == 'C S R'){
+                break;
+            case 'C S R':
                 say = 'C S R stands for Customer Service Representative';
-            }
-            else if (acronym == 'D C R'){
+                break;
+            case 'D C R':
                 say = 'D C R stands for Document Repository';
-            }
-            else if (acronym == 'D D I'){
+                break;
+            case 'D D I':
                 say = 'D D I stands for Dynamic Data Integrator';
-            }
-            else if (acronym == 'E M R'){
+                break;
+            case 'E M R':
                 say = 'E M R stands for Elastic Map Reduce';
-            }
-            else if (acronym == 'E R'){
+                break;
+            case 'E R':
                 say = 'E R stands for Enterprise Release';
-            }
-            else if (acronym == 'E R P'){
+                break;
+            case 'E R P':
                 say = 'E R P stands for Enterprise Resource Planning';
-            }
-            else if (acronym == 'E R T'){
+                break;
+            case 'E R T':
                 say = 'E R T stands for Energy Resource Transmission';
-            }
-            else if (acronym == 'F O'){
+                break;
+            case 'F O':
                 say = 'F O stands for Front Office';
-            }
-            else if (acronym == 'F R'){
+                break;
+            case 'F R':
                 say = 'F R stands for Functinoal Requirements';
-            }
-            else if (acronym == 'F R R B'){
+                break;
+            case 'F R R B':
                 say = 'F R R B stands for Funding Release Review Board';
-            }
-            else if (acronym == 'F T E'){
+                break;
+            case 'F T E':
                 say = 'F T E stands for Full Time Employment';
-            }
-            else if (acronym == 'I M'){
+                break; 
+            case 'I M':
                 say = 'I M stands for Infrastruction Management';
-            }
-            else if (acronym == 'I R D'){
+                break;
+            case 'I R D':
                 say = 'I R D stands for Infrastructure Requirements Document';
-            }
-            else if (acronym == 'I S O'){
+                break;
+            case 'I S O':
                 say = 'I S O stands for Infrastructure Support Office';
-            }
-            else if (acronym == 'I V R'){
+                break;
+            case 'I V R':
                 say = 'I V R stands for Interactive Voice Response';
-            }
-            else if (acronym == 'L O B'){
+                break;
+            case 'L O B':
                 say = 'L O B stands for Line of Business';
-            }
-            else if (acronym == 'M M'){
+                break;
+            case 'M M':
                 say = 'M M stands for Material Management';
-            }
-            else if (acronym == 'N F R'){
+                break;
+            case 'N F R':
                 say = 'N F R stands for Non-Functional Requirements';
-            }
-            else if (acronym == 'O G C'){
+                break;
+            case 'O G C':
                 say = 'O G C stands for Ongoing Costs';
-            }
-            else if (acronym == 'O L'){
+                break;
+            case 'O L':
                 say = 'O L stands for Online Release';
-            }
-            else if (acronym == 'P A'){
+                break;
+            case 'P A':
                 say = 'P A stands for Production Acceptance';
-            }
-            else if (acronym == 'P C I'){
+                break;
+            case 'P C I':
                 say = 'P C I stands for Project Cost Index';
-            }
-            else if (acronym == 'P C R'){
+                break;
+            case 'P C R':
                 say = 'P C R stands for Personal Change Request';
-            }
-            else if (acronym == 'P I I'){
+                break;
+            case 'P I I':
                 say = 'P I I stands for Personal Identifying Information';
-            }
-            else if (acronym == 'P I R'){
+                break;
+            case 'P I R':
                 say = 'P I R stands for Process Improvement Request';
-            }
-            else if (acronym == 'P O'){
+                break;
+            case 'P O':
                 say = 'P O stands for Purchase Order';
-            }
-            else if (acronym == 'P O C'){
+                break;
+            case 'P O C':
                 say = 'P O C stands for Proof of Concept';
-            }
-            else if (acronym == 'P P Q A'){
+                break;
+            case 'P P Q A':
                 say = 'P P Q A stands for Process and Product Quality Assurance';
-            }
-            else if (acronym == 'P S I'){
+                break;
+                
+            case 'P S I':
                 say = 'P S I stands for Project Schedule Index';
-            }
-            else if (acronym == 'P S R'){
+                break;
+            case 'P S R':
                 say = 'P S R stands for Project Status Report';
-            }
-            else if (acronym == 'P T'){
+                break;
+            case 'P T':
                 say = 'P T stands for Performance Testing';
-            }
-            else if (acronym == 'R C A'){
+                break;
+            case 'R C A':
                 say = 'R C A stands for Root Cause Analysis';
-            }
-            else if (acronym == 'R I C E F W'){
+                break;
+            case 'R I C E F W':
                 say = 'R I C E F W stands for Reports, Interface, Conversation, Enhancements, Forms, and Workflow';
-            }
-            else if (acronym == 'R N C'){
+                break;
+            case 'R N C':
                 say = 'R N C stands for Risk and Contingency';
-            }
-            else if (acronym == 'R O M'){
+                break;
+            case 'R O M':
                 say = 'R O M stands for Rough Order of Magnitude';
-            }
-            else if (acronym == 'R P A'){
+                break;
+            case 'R P A':
                 say = 'R P A stands for Robotic Process Automation';
-            }
-            else if (acronym == 'S L A'  ){
+                break;
+            case 'S L A':
                 say = 'S L A stands for Service Level Agreement';
-            }
-            else if (acronym == 'S L O'){
+                break;
+            case 'S L O':
                 say = 'S L O stands for Service Level';
-            }
-            else if (acronym == 'S L T'){
+                break;
+            case 'S L T':
                 say = 'S L T stands for Service Level';
-            }
-            else if (acronym == 'S M E'){
+                break;
+            case 'S M E':
                 say = 'S M E stands for Subject Matter Experts';
-            }
-            else if (acronym == 'S N'){
+                break;
+            case 'S N':
                 say = 'S N stands for Service Now';
-            }
-            else if (acronym == 'S O W'){
+                break;
+            case 'S O W':
                 say = 'S O W stands for Statement of Work';
-            }
-            else if (acronym == 'S R M'){
+                break;
+            case 'S R M':
                 say = 'S R M stands for Strategic Relationship Manager';
-            }
-            else if (acronym == 'T A R'){
+                break;
+            case 'T A R':
                 say = 'T A R stands for Tasks, Authority, Responsibilities';
-            }
-            else if (acronym == 'T D D'){
+                break;
+            case 'T D D':
                 say = 'T D D stands for Test Driven Development';
-            }
-            else if (acronym == 'U A T'){
+                break;
+            case 'U A T':
                 say = 'U A T stands for User Acceptance Testing';
-            }
-            else if (acronym == 'U P O'){
+                break;
+            case 'U P O':
                 say = 'U P O stands for Unplanned Outage';
-            }
-            else if (acronym == 'W B S'){
+                break;
+            case 'W B S':
                 say = 'W B S stands for Work Breakdown Structure';
-            }
-            else if (acronym == 'W M'){
+                break;
+            case 'W M':
                 say = 'W M stands for Work Management';
-            }
-            else if (acronym == 'X I'){
+                break;
+            case 'X I':
                 say = 'X I stands for Exchange Infrastructure';
-            }
-            else{
-                say = 'Sorry, I do not know that acronym';
-            }
+                break;
+        }
             
           this.emit(':tell', say);  
         }
+
+    };
